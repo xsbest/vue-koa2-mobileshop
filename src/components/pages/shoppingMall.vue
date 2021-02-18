34012be043910acdcb536150e8654d8498deec04
Xiongl09 <Xiongl09@mingyuanyun.com>
2021-02-18 17:53:35 +0800

    feat: 楼层组件封装实现

--- src/components/pages/shoppingMall.vue
+++ src/components/pages/shoppingMall.vue
@@ -3,87 +3,134 @@
     <div class="search-bar">
       <van-row>
         <van-col span="3">
-          <img :src="locationIcon"
-               class="location-icon"
-               width="60%"
-               alt="">
+          <img :src="locationIcon" class="location-icon" width="60%" alt="" />
         </van-col>
-        <van-col span="16"
-                 class="search-input"></van-col>
+        <van-col span="16" class="search-input"></van-col>
         <van-col span="5">
-          <van-button size="mini"
-                      class="search-button">查找</van-button>
+          <van-button size="mini" class="search-button">查找</van-button>
         </van-col>
       </van-row>
     </div>
     <!-- swiper area -->
     <div class="swiper-area">
       <van-swipe autoplay="2000">
-        <van-swipe-item v-for="(item,index) in bannerPicArray"
-                        :key="index">
-          <img v-lazy="item.imageUrl"
-               width="100%"
-               alt="">
+        <van-swipe-item v-for="(item, index) in bannerPicArray" :key="index">
+          <img v-lazy="item.imageUrl" width="100%" alt="" />
         </van-swipe-item>
       </van-swipe>
     </div>
     <!-- type bar -->
     <div class="type-bar">
-      <div v-for="(item,index) in category"
-           :key="index">
-        <img :src="item.image"
-             style="width:3rem"
-             alt="">
-        <div class="cate-name">{{item.mallCategoryName}}</div>
+      <div v-for="(item, index) in category" :key="index">
+        <img :src="item.image" style="width:3rem" alt="" />
+        <div class="cate-name">{{ item.mallCategoryName }}</div>
       </div>
     </div>
     <!-- banner area -->
     <div>
-      <img v-lazy="advBanner"
-           width="100% ">
+      <img v-lazy="advBanner" width="100% " />
     </div>
     <!-- recommend goods area -->
     <div class="recommend-area">
-      <div class="recommend-title">商品推荐</div>
+      <div class="recommend-title">
+        商品推荐
+      </div>
       <div class="recommend-body">
-
+        <swiper :options="swiperOptions">
+          <swiper-slide v-for="(item, index) in recommendGoods" :key="index">
+            <div class="recommend-item">
+              <img :src="item.image" width="80%" />
+              <div>{{ item.goodsName }}</div>
+              <div>
+                ￥{{ item.price | moneyFilter }}(￥{{
+                  item.mallPrice | moneyFilter
+                }})
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
       </div>
     </div>
+    <!-- floor area -->
+    <floor-component
+      :floorData="floor1"
+      :floorTitle="floorName.floor1"
+    ></floor-component>
+    <floor-component
+      :floorData="floor2"
+      :floorTitle="floorName.floor2"
+    ></floor-component>
+    <floor-component
+      :floorData="floor3"
+      :floorTitle="floorName.floor3"
+    ></floor-component>
   </div>
 </template>
 
 <script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import floorComponent from "../component/floorComponent";
+import { toMoney } from "@/filter/moneyFilter.js";
+import "swiper/dist/css/swiper.css";
 export default {
-  data () {
+  data() {
     return {
-      locationIcon: require('../../assets/images/定位.png'),
+      locationIcon: require("../../assets/images/定位.png"),
       bannerPicArray: [
-        { imageUrl: 'http://p0.meituan.net/codeman/33ff80dc00f832d697f3e20fc030799560495.jpg' },
-        { imageUrl: 'https://p1.meituan.net/travelcube/01d2ab1efac6e2b7adcfcdf57b8cb5481085686.png' },
-        { imageUrl: 'http://p0.meituan.net/codeman/daa73310c9e57454dc97f0146640fd9f69772.jpg' },
+        {
+          imageUrl:
+            "http://p0.meituan.net/codeman/33ff80dc00f832d697f3e20fc030799560495.jpg"
+        },
+        {
+          imageUrl:
+            "https://p1.meituan.net/travelcube/01d2ab1efac6e2b7adcfcdf57b8cb5481085686.png"
+        },
+        {
+          imageUrl:
+            "http://p0.meituan.net/codeman/daa73310c9e57454dc97f0146640fd9f69772.jpg"
+        }
       ],
-      category: [
-
-      ],
-      advBanner: ''
+      category: [],
+      advBanner: "",
+      recommendGoods: [],
+      swiperOptions: {
+        slidesPerView: 3
+      },
+      floor1: [],
+      floor2: [],
+      floor3: [],
+      floorName: {}
+    };
+  },
+  filters: {
+    moneyFilter(money) {
+      return toMoney(money);
     }
   },
-  created () {
+  components: { swiper, swiperSlide, floorComponent },
+  created() {
     this.axios({
       url: "http://localhost:3000/data",
-      method: 'get'
-    }).then(res => {
-      if (res.status === 200) {
-        const data = res.data.data
-        this.category = data.category
-        this.advBanner = data.advertesPicture.PICTURE_ADDRESS
-      }
-      console.log(res);
-    }).catch(e => {
-      console.log(e);
+      method: "get"
     })
+      .then(res => {
+        if (res.status === 200) {
+          const data = res.data.data;
+          this.category = data.category;
+          this.advBanner = data.advertesPicture.PICTURE_ADDRESS;
+          this.recommendGoods = data.recommend;
+          this.floor1 = data.floor1;
+          this.floor2 = data.floor2;
+          this.floor3 = data.floor3;
+          this.floorName = data.floorName;
+        }
+        console.log(res);
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -108,7 +155,7 @@ export default {
 }
 .swiper-area {
   clear: both;
-  max-height: 9rem;
+  max-height: 8.5rem;
   overflow: hidden;
 }
 .type-bar {
@@ -133,6 +180,15 @@ export default {
     font-size: 14px;
     padding: 0.2rem;
     color: #d3a900;
+  }
+  .recommend-body {
+    border-bottom: 1px solid #eee;
+  }
+  .recommend-item {
+    width: 99%;
+    border-right: 1px solid #eee;
+    font-size: 12px;
+    text-align: center;
   }
 }
 </style>
---
+++ src/components/swiper/swiperDefault.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <swiper :options="slideOption">
+      <swiper-slide
+        class="swiper-slide"
+        v-for="(item, index) in slide"
+        :key="index"
+      >
+        Slide{{ item }}
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+export default {
+  data() {
+    return {
+      slide: [1, 2, 3, 4, 5, 6],
+      slideOption: {
+        direction: "vertical",
+        pagination: {
+          el: ".swiper-pagination"
+        }
+      }
+    };
+  },
+  components: { swiper, swiperSlide }
+};
+</script>
+
+<style lang="scss" scoped>
+.swiper-slide {
+  height: 4rem;
+  text-align: center;
+  padding-top: 3rem;
+  border-bottom: 1px solid #ccc;
+}
+</style>
